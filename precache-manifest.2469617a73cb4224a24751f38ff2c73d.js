4c186db311230ec40945ca86d5cc83b26aaa560b
AmeyKhoje <ameykhoje@gmail.com>
2020-12-05 12:03:18 +0530

    Updates

---
+++ precache-manifest.2469617a73cb4224a24751f38ff2c73d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef199123332834ee4780cc7781a4b28e",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "bfb6e089aa123749b03c",
+    "url": "/my-app/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "e0dbb8c33677ce0b847e",
+    "url": "/my-app/static/css/main.630ba46d.chunk.css"
+  },
+  {
+    "revision": "bfb6e089aa123749b03c",
+    "url": "/my-app/static/js/2.8a8c473f.chunk.js"
+  },
+  {
+    "revision": "0bf38cf6c0b51ec69d7a52e8875a6dbf",
+    "url": "/my-app/static/js/2.8a8c473f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0dbb8c33677ce0b847e",
+    "url": "/my-app/static/js/main.27142a2a.chunk.js"
+  },
+  {
+    "revision": "0725322b7bccb4790ba5",
+    "url": "/my-app/static/js/runtime-main.cdc9629e.js"
+  }
+]);
